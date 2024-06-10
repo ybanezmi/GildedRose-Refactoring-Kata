62f622d421f554c246b8f0d2052a80d6c4b157d0
Michelle Ybanez <ybanez.mich@gmail.com>
2024-06-10 11:21:57 +0200

    refactor(swift): extract method for updating item

--- swift/Sources/GildedRose/GildedRose.swift
+++ swift/Sources/GildedRose/GildedRose.swift
@@ -7,51 +7,55 @@ public class GildedRose {
 
     public func updateQuality() {
         for index in 0 ..< items.count {
-            if items[index].name != "Aged Brie" && items[index].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[index].quality > 0 {
-                    if items[index].name != "Sulfuras, Hand of Ragnaros" {
-                        items[index].quality = items[index].quality - 1
+            updateItem(at: index)
+        }
+    }
+    
+    private func updateItem(at index: Int) { 
+        if items[index].name != "Aged Brie" && items[index].name != "Backstage passes to a TAFKAL80ETC concert" {
+            if items[index].quality > 0 {
+                if items[index].name != "Sulfuras, Hand of Ragnaros" {
+                    items[index].quality = items[index].quality - 1
+                }
+            }
+        } else {
+            if items[index].quality < 50 {
+                items[index].quality = items[index].quality + 1
+
+                if items[index].name == "Backstage passes to a TAFKAL80ETC concert" {
+                    if items[index].sellIn < 11 {
+                        if items[index].quality < 50 {
+                            items[index].quality = items[index].quality + 1
+                        }
                     }
+
+                    if items[index].sellIn < 6 {
+                        if items[index].quality < 50 {
+                            items[index].quality = items[index].quality + 1
+                        }
+                    }
+                }
+            }
+        }
+
+        if items[index].name != "Sulfuras, Hand of Ragnaros" {
+            items[index].sellIn = items[index].sellIn - 1
+        }
+
+        if items[index].sellIn < 0 {
+            if items[index].name != "Aged Brie" {
+                if items[index].name != "Backstage passes to a TAFKAL80ETC concert" {
+                    if items[index].quality > 0 {
+                        if items[index].name != "Sulfuras, Hand of Ragnaros" {
+                            items[index].quality = items[index].quality - 1
+                        }
+                    }
+                } else {
+                    items[index].quality = items[index].quality - items[index].quality
                 }
             } else {
                 if items[index].quality < 50 {
                     items[index].quality = items[index].quality + 1
-
-                    if items[index].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[index].sellIn < 11 {
-                            if items[index].quality < 50 {
-                                items[index].quality = items[index].quality + 1
-                            }
-                        }
-
-                        if items[index].sellIn < 6 {
-                            if items[index].quality < 50 {
-                                items[index].quality = items[index].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if items[index].name != "Sulfuras, Hand of Ragnaros" {
-                items[index].sellIn = items[index].sellIn - 1
-            }
-
-            if items[index].sellIn < 0 {
-                if items[index].name != "Aged Brie" {
-                    if items[index].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[index].quality > 0 {
-                            if items[index].name != "Sulfuras, Hand of Ragnaros" {
-                                items[index].quality = items[index].quality - 1
-                            }
-                        }
-                    } else {
-                        items[index].quality = items[index].quality - items[index].quality
-                    }
-                } else {
-                    if items[index].quality < 50 {
-                        items[index].quality = items[index].quality + 1
-                    }
                 }
             }
         }
